73bf869cffa42287c17f77d70f64f942ca34168d
unknown <niganphantomhive@icloud.com>
2023-10-25 23:28:52 +0500

    work with treeViewObjectCommunications

--- Electronic_School_Gradebook/FormAdminPanel.cs
+++ Electronic_School_Gradebook/FormAdminPanel.cs
@@ -104,63 +104,63 @@ namespace Electronic_School_Gradebook
 				}
 
 			}
-			//else if (checkBoxStudents.Checked)
-   //         {
-			//	int countClasses = dBTools.countRows("Classes");
-			//	int countStudents = dBTools.countRows("Students");
-				
-			//	nodeConnects = new NodeConnect[countClasses + countStudents];
-			//	for (int i = 0; i < treeClasses.Length; i++)
-			//	{
-			//		object[,] dataStudents = dBTools.executeSelectTable($"select ID_Student, Name_Student,Surname_Student from Students where ID_Class = {data[i, 0]}");
-						
-			//		TreeNode[] treeStudents = new TreeNode[dataStudents.GetLength(0)];
+            else if (checkBoxStudents.Checked)
+            {
+				object countClasses = dBTools.executeAnySqlScalar("select count(ID_Class) from Classes;");
+				object countStudents = dBTools.executeAnySqlScalar("select count(ID_Student) from Students;");
 
-			//		for (int j = 0; j < dataStudents.GetLength(0); j++)
-			//		{
-			//			treeStudents[j] = new TreeNode((dataStudents[j, 2] + " " + dataStudents[j, 1]).ToString());
-			//			nodeConnects[j].node = treeStudents[j];
-			//			nodeConnects[j].id = (int)dataStudents[j, 0];
-			//			nodeConnects[j].types = NodeConnect.Types.STUDENT;
-			//		}
+				nodeConnects = new NodeConnect[(int)countClasses + (int)countStudents];
+                for (int i = 0; i < treeClasses.Length; i++)
+                {
+                    object[,] dataStudents = dBTools.executeSelectTable($"select ID_Student, Name_Student,Surname_Student from Students where ID_Class = {data[i, 0]}");
 
-			//		treeClasses[i] = new TreeNode(data[i, 1].ToString(), treeStudents);
-			//		nodeConnects[i].node = treeClasses[i];
-			//		nodeConnects[i].id = (int)data[i, 0];
-			//		nodeConnects[i].types = NodeConnect.Types.CLASS;
-			//		treeViewMainCommunications.Nodes.Add(treeClasses[i]);
-			//	}
+                    TreeNode[] treeStudents = new TreeNode[dataStudents.GetLength(0)];
 
-			//}
-			//else if (checkBoxTeachers.Checked)
-   //         {
-			//	int countClasses = dBTools.countRows("Classes");
-			//	int countTeachers = dBTools.countRows("Teachers", "join Users on Users.ID_User=Teachers.ID_User where LifeStatus=1;");
+                    for (int j = 0; j < dataStudents.GetLength(0); j++)
+                    {
+                        treeStudents[j] = new TreeNode((dataStudents[j, 2] + " " + dataStudents[j, 1]).ToString());
+                        nodeConnects[j].node = treeStudents[j];
+                        nodeConnects[j].id = (int)dataStudents[j, 0];
+                        nodeConnects[j].types = NodeConnect.Types.STUDENT;
+                    }
 
-			//	nodeConnects = new NodeConnect[countClasses + countTeachers];
-			//	for (int i = 0; i < treeClasses.Length; i++)
-			//	{
-					
-			//		object[,] dataTeachers = dBTools.executeSelectTable($"SELECT A.ID_Teacher, A.Name_Teacher, A.Surname_Teacher from Teachers A JOIN TeachToClass B on A.ID_Teacher = B.ID_Teacher join Classes C on B.ID_Class = C.ID_Class where C.ID_Class = {data[i, 0]}");
+                    treeClasses[i] = new TreeNode(data[i, 1].ToString(), treeStudents);
+                    nodeConnects[i].node = treeClasses[i];
+                    nodeConnects[i].id = (int)data[i, 0];
+                    nodeConnects[i].types = NodeConnect.Types.CLASS;
+                    treeViewMainCommunications.Nodes.Add(treeClasses[i]);
+                }
+
+            }
+            else if (checkBoxTeachers.Checked)
+            {
+				object countClasses = dBTools.executeAnySqlScalar("select count(ID_Class) from Classes;");
+				object countTeachers = dBTools.executeAnySqlScalar("select count(ID_Teacher) from Teachers;");
+
+				nodeConnects = new NodeConnect[(int)countClasses + (int)countTeachers];
+                for (int i = 0; i < treeClasses.Length; i++)
+                {
+
+                    object[,] dataTeachers = dBTools.executeSelectTable($"SELECT A.ID_Teacher, A.Name_Teacher, A.Surname_Teacher from Teachers A JOIN TeachToClass B on A.ID_Teacher = B.ID_Teacher join Classes C on B.ID_Class = C.ID_Class where C.ID_Class = {data[i, 0]}");
 
 
-			//		TreeNode[] treeTeach = new TreeNode[dataTeachers.GetLength(0)];
+                    TreeNode[] treeTeach = new TreeNode[dataTeachers.GetLength(0)];
 
-			//		for (int j = 0; j < dataTeachers.GetLength(0); j++)
-			//		{
-			//			treeTeach[j] = new TreeNode((dataTeachers[j,2] + " " + dataTeachers[j, 1]).ToString());
-			//			nodeConnects[j].node = treeTeach[j];
-			//			nodeConnects[j].id = (int)dataTeachers[j, 0];
-			//			nodeConnects[j].types = NodeConnect.Types.TEACHER;
-			//		}
+                    for (int j = 0; j < dataTeachers.GetLength(0); j++)
+                    {
+                        treeTeach[j] = new TreeNode((dataTeachers[j, 2] + " " + dataTeachers[j, 1]).ToString());
+                        nodeConnects[j].node = treeTeach[j];
+                        nodeConnects[j].id = (int)dataTeachers[j, 0];
+                        nodeConnects[j].types = NodeConnect.Types.TEACHER;
+                    }
 
-			//		treeClasses[i] = new TreeNode(data[i, 1].ToString(), treeTeach);
-			//		nodeConnects[i].node = treeClasses[i];
-			//		nodeConnects[i].id = (int)data[i, 0];
-			//		nodeConnects[i].types = NodeConnect.Types.CLASS;
-			//		treeViewMainCommunications.Nodes.Add(treeClasses[i]);
-			//	}
-			//}
+                    treeClasses[i] = new TreeNode(data[i, 1].ToString(), treeTeach);
+                    nodeConnects[i].node = treeClasses[i];
+                    nodeConnects[i].id = (int)data[i, 0];
+                    nodeConnects[i].types = NodeConnect.Types.CLASS;
+                    treeViewMainCommunications.Nodes.Add(treeClasses[i]);
+                }
+            }
             else
             {
 				object countClasses = dBTools.executeAnySqlScalar("select count(ID_Class) from Classes;");
@@ -192,12 +192,15 @@ namespace Electronic_School_Gradebook
 			FormAdminPanel_Load(sender, e);
 		}
 
-        private void treeViewMainCommunications_AfterSelect(object sender, TreeViewEventArgs e)
+		int BDid = 0;
+		NodeConnect.Types tableType;
+		private void treeViewMainCommunications_AfterSelect(object sender, TreeViewEventArgs e)
         {
-			int BDid = 0;
-			NodeConnect.Types tableType;
+			treeViewObjectCommunications.Nodes.Clear();
+			DBTools dBTools = new DBTools(FormAuthorization.sqlConnection);
 
-			for(int i = 0; i < nodeConnects.Length; i++)
+
+			for (int i = 0; i < nodeConnects.Length; i++)
             {
                 if (nodeConnects[i].node == treeViewMainCommunications.SelectedNode)
                 {
@@ -207,6 +210,64 @@ namespace Electronic_School_Gradebook
             }
 
 			//switch case (по типу таблицы)
+			switch(tableType)
+            {
+				case NodeConnect.Types.CLASS:
+					
+					object[,] dataStudents = dBTools.executeSelectTable($"select ID_Student, Name_Student, Surname_Student from Students where ID_Class = {BDid}");
+					object[,] dataTeachers = dBTools.executeSelectTable($"SELECT A.ID_Teacher, A.Name_Teacher, A.Surname_Teacher from Teachers A JOIN TeachToClass B on A.ID_Teacher = B.ID_Teacher join Classes C on B.ID_Class = C.ID_Class where C.ID_Class = {BDid}");
+
+
+					TreeNode[] treeStudentsTeach = new TreeNode[dataStudents.GetLength(0)+dataTeachers.GetLength(0)];
+
+					for (int j = 0; j < dataStudents.GetLength(0); j++)
+					{
+						treeStudentsTeach[j] = new TreeNode((dataStudents[j, 2] + " " + dataStudents[j, 1]).ToString());
+						treeViewObjectCommunications.Nodes.Add(treeStudentsTeach[j]);
+					}
+
+                    for (int k = dataStudents.GetLength(0), z = 0; k < treeStudentsTeach.Length; z++, k++)
+                    {
+                        treeStudentsTeach[k] = new TreeNode((dataTeachers[z, 2] + " " + dataTeachers[z, 1]).ToString());
+                        treeViewObjectCommunications.Nodes.Add(treeStudentsTeach[k]);
+
+                    }
+
+                    break;
+
+				case NodeConnect.Types.STUDENT:
+					object[,] dataParent = dBTools.executeSelectTable($"SELECT A.ID_Parent, A.Name_Parent, A.Surname_Parent from Parents A JOIN ParentToStud B on A.ID_Parent = B.ID_ParentToStud join Students C on B.ID_Student = C.ID_Student where C.ID_Student = {BDid}");
+					TreeNode[] treeParent = new TreeNode[dataParent.GetLength(0)];
+
+					for (int j = 0; j < treeParent.Length; j++)
+					{
+						treeParent[j] = new TreeNode((dataParent[j, 2] + " " + dataParent[j, 1]).ToString());
+
+						treeViewObjectCommunications.Nodes.Add(treeParent[j]);
+					}
+
+					break;
+
+				case NodeConnect.Types.TEACHER:
+					object[,] dataClasses = dBTools.executeSelectTable($"SELECT A.ID_Class, A.Name_Class from Classes A JOIN TeachToClass B on A.ID_Class = B.ID_Class join Teachers C on B.ID_Teacher = C.ID_Teacher where C.ID_Teacher = {BDid}");
+					TreeNode[] treeClasses = new TreeNode[dataClasses.GetLength(0)];
+					for (int j = 0; j < treeClasses.Length; j++)
+					{
+                        object[,] dataSubj = dBTools.executeSelectTable($"select A.ID_Subject, A.Name_Subject from Subjects A join TeachToSubj B on A.ID_Subject = B.ID_Subject JOIN Teachers C ON B.ID_Teacher = C.ID_Teacher JOIN TeachToClass D ON C.ID_Teacher = D.ID_Teacher join Classes E on D.ID_Class = E.ID_Class WHERE C.ID_Teacher = {BDid} AND D.ID_Class = {dataClasses[j,0]}");
+						TreeNode[] treeeSubj = new TreeNode[dataSubj.GetLength(0)];
+						for(int i = 0; i < treeeSubj.Length; i++)
+                        {
+							treeeSubj[i] = new TreeNode(dataSubj[i, 1].ToString());
+
+                        }
+						treeClasses[j] = new TreeNode(dataClasses[j, 1].ToString(),treeeSubj);
+
+						treeViewObjectCommunications.Nodes.Add(treeClasses[j]);
+					}
+
+					break;
+
+            }
 			
 
         }
